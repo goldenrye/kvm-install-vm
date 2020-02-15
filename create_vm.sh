dfe34318f0de9407e0d33e7450ed8c5cd6d33171
Jianwen Pi <jianwpi@gmail.com>
2020-02-14 16:16:52 -0800

    reorganized scripts with pre-defined varables

--- create_vm.sh
+++ create_vm.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-declare -A vm_ip
+# SHOULD BE MOVED TO CONFIG FILE !!!
+VM_TOTAL=2
+VM_PREFIX=asn
+VM_IP_PREFIX=100.1.1
+VM_USER=secnet
+VM_DISTRO=asn
+#VM_DIR=/tmp/vm/vm
+VM_DIR=/home/`whoami`/Work/vm
+# SHOULD BE MOVED TO CONFIG FILE !!!
+
 declare -i num
 declare -i ip
-declare -i vm_total
+declare -A vm_ips
+declare -A vm_namess
 
-VM_DIR=$1
+#VM_DIR=$1
 
 function help {
     echo "$0 <VM_DIR>"
@@ -18,33 +28,48 @@ fi
 if [ ! -d ${VM_DIR} ]; then
     mkdir ${VM_DIR}
 fi
+if [ ! -f ${VM_DIR}/asn-vm.img ]; then
+    echo "Please copy the VM image to ${VM_DIR} first"
+    exit
+fi
 
 ifconfig br-data > /dev/null
 if [ "$?" != "0" ]; then
-    sudo brctl addbr br-ctrl
+    stotaudo brctl addbr br-ctrl
     sudo ifconfig br-ctrl up
     sudo brctl addbr br-data
     sudo ifconfig br-data up
 fi
 
-vm_total=1
+echo "***To create [${VM_TOTAL}] VMs under [${VM_DIR}]..."
 num=0
-while [ $num -lt ${vm_total} ]; do
-    vm_suffix=$(printf "%02x" $num)
+while [ $num -lt ${VM_TOTAL} ]; do
+    vm_mac=$(printf "%02x" $num)
+
     ip=num+100
-    echo "**Create vm secnet-vm${vm_suffix}..."
-    . ./kvm-install-vm create -l ${VM_DIR} -L ${VM_DIR} -t secnet -s `pwd`/network-provision.sh -T US/Pacific -M ${vm_suffix} -I 192.168.122.$ip -u secnet -v secnet-vm${vm_suffix}
-    vm_ip[$num]=192.168.122.$ip
+    vm_ip=${VM_IP_PREFIX}.${ip}
+
+    vm_suffix=$(printf "%03d" $num)
+    vm_name=${VM_PREFIX}-${vm_suffix}
+
+    echo "**Creating VM:[${vm_name}]  DISTRO:[${VM_DISTRO}]  IP:[${vm_ip}]  MAC_SUFFIX:[${vm_mac}]..."
+
+    ./kvm-install-vm create -l ${VM_DIR} -L ${VM_DIR} -t ${VM_DISTRO} \
+		     -s `pwd`/network-provision.sh -T US/Pacific -M ${vm_mac} \
+		     -I ${vm_ip} -u ${VM_USER} -v ${vm_name}
+
+    vm_names[$num]=${vm_name}
+    vm_ips[$num]=${vm_ip}
     num=num+1
 done
 
-:>vm_file
-echo "vm_name        vm_ip"
+echo ""
+echo -e "VM_NAME\t\t IP"
 echo "-----------------------------"
 num=0
-while [ $num -lt ${vm_total} ]; do
-    vm_suffix=$(printf "%02x" $num)
-    echo "secnet-vm${vm_suffix}:   ${vm_ip[$num]}"
-    echo "secnet-vm${vm_suffix}:   ${vm_ip[$num]}" >> vm_file
+while [ $num -lt ${VM_TOTAL} ]; do
+    vm_suffix=$(printf "%03d" $num)
+    echo -e "${vm_names[$num]}:\t\t ${vm_ips[$num]}"
+#    echo "${vm_names[$num]}:\\t  ${vm_ips[$num]}" >> vm_file
     num=num+1
 done
--- destroy_vm.sh
+++ destroy_vm.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# SHOULD BE MOVED TO CONFIG FILE !!!
+VM_TOTAL=10
+VM_PREFIX=asn
+VM_DIR=/home/`whoami`/Work/vm
+# SHOULD BE MOVED TO CONFIG FILE !!!
+
 declare -i num
 
-VM_DIR=$1
+#VM_DIR=$1
 
 function help {
     echo "$0 <VM_DIR>"
@@ -16,15 +22,25 @@ if [ ! -d ${VM_DIR} ]; then
     mkdir ${VM_DIR}
 fi
 
+echo "***To destroy [${VM_TOTAL}] VMs under [${VM_DIR}]..."
+
 num=0
-while [ $num -lt 2 ]; do
-    vm_suffix=$(printf "%02x" $num)
-    vmname=secnet-vm${vm_suffix}
-    echo "**Delete vm ${vmname}..."
-    virsh destroy $vmname
-    virsh undefine $vmname
-    virsh pool-destroy $vmname
-    rm -fr ${VM_DIR}/$vmname
+while [ $num -lt ${VM_TOTAL} ]; do
+    vm_suffix=$(printf "%03d" $num)
+    vm_name=${VM_PREFIX}-${vm_suffix}
+
+    echo "**Destroying VM:[${vm_name}]..."
+    #   echo "**Delete vm ${vmname}..."
+    if [ ! -d ${VM_DIR}/$vm_name ]; then
+	echo "  ${VM_DIR}/$vm_name doesn't exist, skipped"
+	num=num+1
+	continue
+    fi
+
+    virsh destroy $vm_name
+    virsh undefine $vm_name
+    virsh pool-destroy $vm_name
+    rm -fr ${VM_DIR}/$vm_name
 
     num=num+1
 done
